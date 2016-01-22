0a11ab79ce1bdf7788f60700bb60e6895ba10f42
polish <polishcivil@gmail.com>
2016-01-22 02:38:03 +0100

    Little packet decoder change.

--- app/src/main/scala/pl/jaca/server/networking/PacketDecoder.scala
+++ app/src/main/scala/pl/jaca/server/networking/PacketDecoder.scala
@@ -9,24 +9,51 @@ import pl.jaca.server.Session
 import pl.jaca.server.networking.PacketReader._
 
 /**
- * @author Jaca777
- *         Created 2015-06-13 at 13
- */
+  * @author Jaca777
+  *         Created 2015-06-13 at 13
+  */
 class PacketDecoder(resolver: => PacketResolver) extends ByteToMessageDecoder {
+  val STATE_SIZE = 0
+  val STATE_OPCODE = 1
+  val STATE_PAYLOAD = 2
 
-  override def decode(ctx: ChannelHandlerContext, in: ByteBuf, out: util.List[AnyRef]) =
-    if (isReadable(in)) tryToRead(ctx, in, out)
+  var state = STATE_SIZE
+  var size = 0.toShort
+  var opcode = 0.toShort
 
-
-  private def tryToRead(ctx: ChannelHandlerContext, in: ByteBuf, out: util.List[AnyRef]) {
-    val size = readSize(in)
-    if (isComplete(in, size)) readPacket(ctx, in, out, size)
+  override def decode(ctx: ChannelHandlerContext, in: ByteBuf, out: util.List[AnyRef]): Unit = {
+    if (in.isReadable(2)) {
+      state match {
+        case STATE_SIZE =>
+          size = in.readShort()
+          state = STATE_OPCODE
+        case STATE_OPCODE =>
+          opcode = in.readShort()
+          state = STATE_PAYLOAD
+        case STATE_PAYLOAD =>
+          if (in.isReadable(size - 4)) {
+            val data: Array[Byte] = readData(in, size, opcode)
+            val partialPacket = (s: Session) => resolver.resolve(opcode, size, data, s)
+            out.add(partialPacket)
+            state = STATE_SIZE
+          }
+      }
+    }
+    //getInt(0) -> bad
+    //    if (isReadable(in))
+    //      tryToRead(ctx, in, out)
   }
 
-  private def readPacket(ctx: ChannelHandlerContext, in: ByteBuf, out: util.List[AnyRef], size: Short) {
-    val id = readId(in, size)
-    val bytes: Array[Byte] = readData(in, size, id)
-    val partialPacket = (s: Session) => resolver.resolve(id, size, bytes, s)
-    out.add(partialPacket)
-  }
+
+  //  private def tryToRead(ctx: ChannelHandlerContext, in: ByteBuf, out: util.List[AnyRef]) {
+  //    val size = readSize(in) + 2
+  //    if (isComplete(in, size.toShort)) readPacket(ctx, in, out, size.toShort)
+  //  }
+  //
+  //  private def readPacket(ctx: ChannelHandlerContext, in: ByteBuf, out: util.List[AnyRef], size: Short) {
+  //    val id = readId(in, size)
+  //    val bytes: Array[Byte] = readData(in, size, id)
+  //    val partialPacket = (s: Session) => resolver.resolve(id, size, bytes, s)
+  //    out.add(partialPacket)
+  //  }
 }
